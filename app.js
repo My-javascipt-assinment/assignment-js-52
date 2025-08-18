213f61be3638a5dea1581e310ed44dce35feb565
Azizullah <azizullahcodes@gmail.com>
2025-08-18 06:32:18 -0700

    u

--- app.js
+++ app.js
@@ -351,7 +351,7 @@ Enhanced object literals
 // }
 // console.log(details)
 /*==================================== Question No 21 =========================================
-
+sum array each value sqare of array
 ======================================= Answer =====================================*/
 // let num = [1,2,3];
 // // let need = num.map((element)=>{return element*element;});
@@ -368,99 +368,185 @@ Enhanced object literals
 // for(let i = 0;i < arr.length;i++){sum = sum + arr[i]};
 // console.log(sum)
 
+/*==================================== Question No 22 =========================================
+find minimum value
+======================================= Answer =====================================*/
+// function processing(){let getValue = document.getElementById('input').value;
+   
+//     // first we convert getValues in array by split
+//     getValue = getValue.split(' ');
+//     console.log(getValue);
+//     let minimum = getValue[0];
+//     for(let i = 0;i < getValue.length; i++){if(getValue[i] < minimum){minimum = getValue[i]}}
+//     document.getElementById('display').innerHTML = minimum;
+// }
+
+
+
+
+/*==================================== Question No 23 =========================================
+convert camelcase
+======================================= Answer =====================================*/
+// function camelcasing(){let getInput = document.getElementById('userinput').value;
+//     getInput = getInput.toLowerCase();
+//    getInput = getInput.split(' ');
+//   const toCamelCase = getInput.map((item, index)=>{if(index === 0){return item;}
+// else{return item.charAt(0).toUpperCase()+item.slice(1)}})
+// let finalResult = toCamelCase.join('');
+// document.getElementById('result').innerHTML = finalResult;
+    
+// }
+
+
+
+
+/*==================================== Question No 24 =========================================
+check whether string is uppercase or lowercase
+======================================= Answer =====================================*/
+
+// let str = prompt('enter word')
+// if(str === str.toUpperCase()){console.log('uppercs')}
+// else{console.log('lower')}
+
+
+/*==================================== Question No 25 =========================================
+find average
+======================================= Answer =====================================*/
+// function process(){let getValue = document.getElementById('input').value;
+//   getValue = getValue.split(' ');
+//   let length = getValue.length;
+//   let sum = 0;
+//   for(let key of getValue){sum = sum + Number(key);}
+//   let mean = sum/length;
+//   document.getElementById('display').innerHTML = mean;
+// }
+
+
+/*==================================== Question No 26 =========================================
+find medean numbers
+======================================= Answer =====================================*/
+// function processing() {
+//   let get = document.getElementById('input').value;
+
+//   // Convert input into array of numbers
+//   let arr = get.trim().split(' ').map(Number);
+
+//   // Sort numbers
+//   arr.sort((a, b) => a - b);
+//   console.log("Sorted:", arr);
+
+//   let median;
+
+//   if (arr.length % 2 !== 0) {
+//     // Odd → middle element
+//     let ind = Math.floor(arr.length / 2);
+//     median = arr[ind];
+//   } else {
+//     // Even → average of middle two
+//     let index1 = arr.length / 2 - 1;
+//     let index2 = arr.length / 2;
+//     median = (arr[index1] + arr[index2]) / 2;
+//   }
+
+//   console.log("Median:", median);
+// }
+
+
+/*==================================== Question No 27 =========================================
+rest parameter
+======================================= Answer =====================================*/
+// function showData(user1,user2,...rest){console.log(rest);
+//     console.log(user1);
+//     console.log(user2)
+// }
+// showData('apple','mango','banana','grapes','orange')
+
+/*==================================== Question No 28 =========================================
+default parameter
+======================================= Answer =====================================*/
+// function calculation(a,b=5){let ans = a + b;
+//     console.log(ans)
+// }
+// calculation(2,2)
+
+
+
+/*==================================== Question No 29 =========================================
+promise
+======================================= Answer =====================================*/
+// const myPromise = new Promise((resolve, reject)=>{let num = prompt('enter num');
+//     if(num % 2 === 0){resolve('Number is even')}
+//     else{reject('No is not even')}
+// })
+
+// myPromise 
+// .then((resolve)=>{console.log(resolve)})
+// .catch((reject)=>{console.log(reject)})
+
+
+/*==================================== Question No 30 =========================================
+
+======================================= Answer =====================================*/
+// const numbers = [1,2,2,3,1,4,2];
+// let num1 = numbers.filter((item)=>{return item === 1})
+// console.log(num1)
+// let num2 = numbers.filter((item)=>{return item === 2})
+// console.log(num2)
+// let num3 = numbers.filter((item)=>{return item === 3})
+// console.log(num3)
+// let num4 = numbers.filter((item)=>{return item === 4})
+// console.log(num4)
+// let a = num1.at(0);
+// let aValue = num1.length;
+// let b = num2.at(0);
+// let bValue = num2.length;
+// let c = num3.at(0);
+// let cValue = num3.length;
+// let d = num4.at(0);
+// let dValue = num1.length;
+// let obj = {'a': aValue,
+//     'b': bValue,
+//     'c': cValue,
+//     'd': dValue
+// }
+// console.log(obj)
+// const numbers = ['apple','mango','apple','mango','bnanan','apple'];
+// let counts ={}
+// for(let element of numbers){counts[element] = (counts[element]|| 0)+1;}
+// console.log(counts)
+
+/*==================================== Question No 1 =========================================
+find mode
+======================================= Answer =====================================*/
+
+// function findMode(numbers) {
+//   let counts = {};
+//   let maxCount = 0;
+//   let mode = []; // Use an array to store all modes, in case of a tie
+
+//   for (let item of numbers) {
+//     counts[item] = (counts[item] || 0) + 1;
+    
+//     // If the current item's count is higher than the max, it's the new mode
+//     if (counts[item] > maxCount) {
+//       maxCount = counts[item];
+//       mode = [item]; // Reset mode array with this new item
+      
+//     // If the count is equal to the max, it's a tie, so add it to the list
+//     } else if (counts[item] === maxCount) {
+//       mode.push(item);
+//     }
+//   }
+
+//   // Return the result
+//   return mode;
+// }
+
+// let ans = findMode([1,2,6,2,7,6]);
+// console.log(ans); // This will correctly log [2, 5]
 /*==================================== Question No 1 =========================================
 
 ======================================= Answer =====================================*/
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
-
-
-
-/*==================================== Question No 1 =========================================
-
-======================================= Answer =====================================*/
-
-
-
 
 
 
